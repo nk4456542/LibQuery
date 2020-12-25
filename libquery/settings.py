a4ca51fdc5f2d7dc8ea94b9738908a094980f280
Zaheer Abbas <nk4456542@gmail.com>
2020-12-25 16:14:02 +0530

    Final Version

--- libquery/settings.py
+++ libquery/settings.py
@@ -21,10 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#@!7q$^^7dx!1d24@8))e9h6k@^14$il&(!9yiqgew6ub0&up5'
+# SECRET_KEY = '#@!7q$^^7dx!1d24@8))e9h6k@^14$il&(!9yiqgew6ub0&up5'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', '#@!7q$^^7dx!1d24@8))e9h6k@^14$il&(!9yiqgew6ub0&up5')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
