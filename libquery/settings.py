29e9dd4f389b38e5140040445e120cd97b659204
Zaheer Abbas <nk4456542@gmail.com>
2020-12-26 15:08:13 +0530

    Update Allowed Hosts

--- libquery/settings.py
+++ libquery/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get(
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://peaceful-sierra-60610.herokuapp.com/','127.0.0.1']
 
 # Application definition
 
